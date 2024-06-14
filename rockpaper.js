3d596cc2a9393435b51d6409a311bbc877365a6c
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-06-14 21:49:50 +0200

    Add humanChoice function in the script
    the script uses the prompt function to get user input
    use the swicth statement to return user's choice or message
    indicating error

--- rockpaper.js
+++ rockpaper.js
@@ -11,7 +11,25 @@ function getComputerChoice() {
             return "Paper"
         case 3:
             return "Scissors"
+
     }
 }
-let comChoice = getComputerChoice()
+
+
+function getHumanChoice() {
+    let humanChoice = +prompt("Give a number between 1 and 3!")
+    switch (humanChoice) {
+        case 1:
+            return "Rock"
+        case 2:
+            return "Paper"
+        case 3:
+            return "Scissors"
+
+        default:
+            return"The choice is wrong ,put numbers between 1 and 3"
+            
+    }
+}
+let comChoice = getHumanChoice()
 console.log(comChoice)
