e1a888d8717e378309828485fb6f520590214e91
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-06-14 21:15:48 +0200

    Add function "getComputerChoice" in the script
    1.generate a random number using Math.random
    2.Convert that number to one of choices using
    switch statement and return that choice as a function result

---
+++ rockpaper.js
@@ -0,0 +1,17 @@
+
+
+
+function getComputerChoice() {
+
+    let computerChoice = Math.floor(Math.random() * 3) + 1
+    switch (computerChoice) {
+        case 1:
+            return "Rock"
+        case 2:
+            return "Paper"
+        case 3:
+            return "Scissors"
+    }
+}
+let comChoice = getComputerChoice()
+console.log(comChoice)
